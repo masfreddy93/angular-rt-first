00e5c636aed78dc8b2610abdbfcc91228a2eb602
Nicola Masiero <103143493+masfreddy93@users.noreply.github.com>
2023-01-10 21:42:45 +0100

    add onInit routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-rt-first';
+export class AppComponent implements OnInit{
+  isHome = true
+
+  constructor(private _router: Router){ }
+
+  ngOnInit(){
+    //tramite il metodo navigate della classe Router posso dire alla mia app di aprire la rotta 'studenti/home' 
+    //non appena l'applicazione viene lanciata (onInit) e dunque anziché andare su 'localhost' va direttamente in
+    //localhost/studenti/home
+    this._router.navigate(['studenti/home']);
+  }
 }
