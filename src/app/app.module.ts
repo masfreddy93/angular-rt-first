5dd2eca1c7cd0c9ecd2ffd0af4120106370abbe9
Nicola Masiero <103143493+masfreddy93@users.noreply.github.com>
2023-01-15 17:15:19 +0100

    add main2 comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Main2Component } from './main2/main2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Main2Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/main2/main2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main2',
+  templateUrl: './main2.component.html',
+  styleUrls: ['./main2.component.scss']
+})
+export class Main2Component {
+
+}
